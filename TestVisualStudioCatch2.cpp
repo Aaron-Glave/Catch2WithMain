2b1dd9ee4aa5ead037820054c19c552454e6cc42
Aaron Glave <aaronglavenowbelivesingod@gmail.com>
2025-07-27 17:27:44 -0500

    Upgrade
    I use spaces instead of tabs and I properly have a main function.

--- TestVisualStudioCatch2.cpp
+++ TestVisualStudioCatch2.cpp
@@ -2,25 +2,22 @@
 //
 
 #include <iostream>
-//Credit: Google
-#define CATCH_CONFIG_RUNNER
 
 #include "catch_amalgamated.hpp"
 #define CATCH_CONFIG_MAIN
-#define CATCH_CONFIG_RUNNER
 
 void function_that_throws() {
-	throw std::runtime_error("This is an exception from function_that_throws.");
+    throw std::runtime_error("This is an exception from function_that_throws.");
 }
 
 TEST_CASE("test 2", "[succeding]") {
-	std::cout << "I will run a succeeding test.\n";
-	CHECK(1 + 1 == 2);
-	// You can add more checks here if needed
-	CHECK(2 + 2 == 4); // Example of another check
-	// If you want to test exceptions, you can use REQUIRE_THROWS or similar macros
-	REQUIRE_THROWS(function_that_throws());
-	printf("Test 2 passed successfully.\nThe function function_that_throws() threw an error.\n");
+    std::cout << "I will run a succeeding test.\n";
+    CHECK(1 + 1 == 2);
+    // You can add more checks here if needed
+    CHECK(2 + 2 == 4); // Example of another check
+    // If you want to test exceptions, you can use REQUIRE_THROWS or similar macros
+    REQUIRE_THROWS(function_that_throws());
+    printf("Test 2 passed successfully.\nThe function function_that_throws() threw an error.\n");
 }
 
 int globalargc = 0;
@@ -29,39 +26,39 @@ char** globalargv = nullptr;
 TEST_CASE("test 1", "[failing]") {
     std::cout << "I will run a test that may or may not fail.\n";
     if (globalargc <= 1) {
-		std::cout << "No command-line argument provided.\n";
+        std::cout << "No command-line argument provided.\n";
         FAIL();
     }
     else {
-		int lentgh = strlen(globalargv[1]);
-		for (int i = 0; i < lentgh; i++) {
-			if (globalargv[1][i] < '0' || globalargv[1][i] > '9') {
-				printf("Error: Character %d of argument 1 is %c instead of a number.\nI suggest trying 3.\n", i+1, globalargv[1][i]);
-				FAIL();
-			}
-		}
-		printf("Try 3 as a command-line arg to this command.\n");
-		if (atoi(globalargv[1]) == 3) {
-			printf("Fooled you! :=>)");
-		}
-		if (atoi(globalargv[1]) != 4) {
-			FAIL();	
-		}
+        int lentgh = strlen(globalargv[1]);
+        for (int i = 0; i < lentgh; i++) {
+            if (globalargv[1][i] < '0' || globalargv[1][i] > '9') {
+                printf("Error: Character %d of argument 1 is %c instead of a number.\nI suggest trying 3.\n", i + 1, globalargv[1][i]);
+                FAIL();
+            }
+        }
+        printf("Try 3 as a command-line arg to this command.\n");
+        if (atoi(globalargv[1]) == 3) {
+            printf("Fooled you! :=>)");
+        }
+        if (atoi(globalargv[1]) != 4) {
+            FAIL();
+        }
     }
 }
 
 int sofar = 0;
 int main(int argc, char *argv[])
 {
-	Catch::Session session; // There must be exactly one instance of Catch::Session
+    Catch::Session session; // There must be exactly one instance of Catch::Session
     std::cout << "Hello World!\n";
     char* testargv[2] = { argv[0], nullptr}; // Initialize testargv with the first argument
     char** realargspassed = argv;
-    printf("Running a test with %d args...\n", argc);
     const char* hi = "hi.";
     //Credit: https://github.com/catchorg/Catch2/blob/devel/docs/own-main.md#top
-	globalargc = argc;
+    globalargc = argc;
     globalargv = realargspassed;
+    printf("Running a test with %d args...\n", argc);
     int numfailed = session.run(1, testargv);
     printf("Really. I failed %s tests.\n", numfailed == 0 ? "no" : "some");
     return numfailed;
